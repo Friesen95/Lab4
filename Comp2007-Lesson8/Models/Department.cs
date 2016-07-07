7e859beb0d783d4f32eb3e1ebea23d12acacf7cd
Alex Friesen <friesen.alex95@gmail.com>
2016-07-07 15:33:42 -0400

    init commit

---
+++ Comp2007-Lesson8/Models/Department.cs
@@ -0,0 +1,29 @@
+namespace Comp2007_Lesson8.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    public partial class Department
+    {
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
+        public Department()
+        {
+            Courses = new HashSet<Cours>();
+        }
+
+        public int DepartmentID { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        [Display(Name ="Name")]
+        public string Name { get; set; }
+
+        public decimal Budget { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<Cours> Courses { get; set; }
+    }
+}
